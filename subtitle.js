7de0419fbcb7bbc4961e49c9de34cb047e2cea8e
ANNA-PC\User <anana96koz@gmail.com>
2018-03-30 11:44:13 +0300

    рабочий выбор слова из субтитров

--- subtitle.js
+++ subtitle.js
@@ -1,49 +1,58 @@
-﻿alert("hello");
-//var curSub;
+﻿
+    alert('hi');
+
+  //  var p = $('p');
+
+    //p.html(function (index, oldHtml) {
+    //    return oldHtml.replace(/\b(\w+?)\b/g, '<span class="word">$1</span>')
+    //    });
+
+  
+
+    //p.click(function (event) { alert(event.target.innerHTML) });
 
 
-var video1 = document.getElementsByTagName("video");
-var trackCur = video1[0].textTracks[0];
-console.log(trackCur);
 
+    //p.mouseover(function (event) { alert(event.target.innerText) });
 
+    var video1 = document.getElementsByTagName("video");
+    var trackCur = video1[0].textTracks[0];
+    console.log(trackCur);
 
-trackCur.oncuechange = function () {
-    console.log("тут");
-    var cue = this.activeCues[0];
-    
-    if (cue !== undefined) {
-        cue.text = "<a href=\"#\" class=\"subt\">" + cue.text + "</a>";
-        console.log(cue.text);
-        var dsp = document.getElementById("display"); // get display area 
-        dsp.innerHTML = "Text: " + cue.text;  // show text  
-        dsp.innerHTML += "<br />StartTime: " + cue.startTime; // show start time 
-        dsp.innerHTML += "<br />End Time: " + cue.endTime;
+    trackCur.oncuechange = function () {
+        var cue = this.activeCues[0];
+        if (cue !== undefined && cue.text[0] != '<') {
+      //      cue.text = "<p>" + cue.text + "</p>";
+            console.log(cue.text);
+            var dsp = document.getElementById("subt"); // get display area 
+
+            
+            dsp.innerHTML = "Text: " + cue.text.replace(/\b(\w+?)\b/g, '<span class="word" onmouseover=Change(this)>$1</span>');
+            cue = undefined;// show text
+            //     dsp.innerHTML += "<br />StartTime: " + cue.startTime; // show start time 
+            //    dsp.innerHTML += "<br />End Time: " + cue.endTime;
+        }
     }
+
+function Change(x) {
+    console.log(x.innerHTML);
 }
 
+    //video1[0].click = function () {
+    //    alert("нажали на видео");
+    //}
+
+   ///video1[0].onmouseover = function () {
+   // //    video1[0].play();
+   // //    // alert("нажали на видео");
+   // //}
+
+   //     function ChangeOver(x) {
+            //    //    alert("навели");
+            //    //  alert(cueCur[i].text);
+            //    video1[0].play()
+            //}
 
 
 
-video1[0].click = function () {
-    alert("нажали на видео");
-}
 
-video1[0].onmouseover = function () {
-    video1[0].play();
-   // alert("нажали на видео");
-}
-
-function ChangeOver(x) {
-//    alert("навели");
-  //  alert(cueCur[i].text);
-    video1[0].play()
-}
-
-
-            //if (tt.activeCues[0] !== undefined) {
-            //    var dynamicHTML = "id: " + tt.activeCues[0].id + ", ";
-            //    dynamicHTML += "text: " + tt.activeCues[0].text + ", ";
-            //    dynamicHTML += "startTime: " + tt.activeCues[0].startTime + ",  ";
-            //    dynamicHTML += "endTime: " + tt.activeCues[0].endTime;
-            //    document.getElementById("insertionPoint").innerHTML += dynamicHTML + "<br/>";
